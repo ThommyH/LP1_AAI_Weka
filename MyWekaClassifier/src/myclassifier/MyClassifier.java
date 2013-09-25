574ae0a49f6fb54bfafca1d7ccc86670e0d0271c
thomahi <thomahi@gmx.de>
2013-09-25 13:34:38 +0200

    erste loc

--- MyWekaClassifier/src/myclassifier/MyClassifier.java
+++ MyWekaClassifier/src/myclassifier/MyClassifier.java
@@ -5,19 +5,54 @@
  */
 package myclassifier;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import weka.classifiers.*;
 import weka.core.*;
 
 public class MyClassifier extends Classifier 
 {
+	private Map<String, Double> values = new HashMap<String, Double>();
     /*
      * This is the method for training the classifier. Add the
      * code needed to train a classifier on the training set,
      * available in the data datastructure.
      */
-    public void buildClassifier(Instances data)
-    {
-  
+    public void buildClassifier(Instances data){
+    	values.put("T", (double)data.numInstances());
+    	System.out.println(data.numAttributes());
+    	Map<String, Integer> values = new HashMap<String, Integer>();
+    	int number_attr = data.numAttributes();
+    	
+    	
+    	int sunny = 0;
+    	int rainy = 0;
+    	int overcast = 0;
+    	int hot = 0;
+    	int mild = 0;
+    	int cool = 0;
+    	int high = 0;
+    	int normal = 0;
+    	int windy = 0;
+    	int not_windy = 0;
+    	
+    	int sunny_yes = 0;
+    	int rainy_yes = 0;
+    	int overcast_yes = 0;
+    	int hot_yes = 0;
+    	int mild_yes = 0;
+    	int cool_yes = 0;
+    	int high_yes = 0;
+    	int normal_yes = 0;
+    	int windy_yes = 0;
+    	int not_wind_yes = 0;
+    	System.out.println(data.instance(0).stringValue(0));
+    		
+    		
+    	
+    	
+    	
     }
     
     /*
@@ -25,8 +60,7 @@ public class MyClassifier extends Classifier
      * Add the code needed to make a classification based on your
      * trained classifier.
      */
-    public double classifyInstance(Instance inst) throws NoSupportForMissingValuesException
-    {
+    public double classifyInstance(Instance inst) throws NoSupportForMissingValuesException {
         int rnd = (int)(Math.random() * 2);
         return rnd;
     }
@@ -36,10 +70,8 @@ public class MyClassifier extends Classifier
      * only thing you might need to change here is the path to
      * the data set.
      */
-    public static void main(String[] args)
-    {
-        try 
-        {
+    public static void main(String[] args){
+        try {
             String[] params = new String[4];
             params[0] = "-t";
             params[1] = "dataset/weather.nominal.arff";
@@ -47,8 +79,7 @@ public class MyClassifier extends Classifier
             params[3] = "10";
             System.out.println(Evaluation.evaluateModel(new MyClassifier(), params));
         }
-        catch (Exception ex) 
-        {
+        catch (Exception ex){
             ex.printStackTrace();
         }
     } 
