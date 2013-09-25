84f50e5425c9caaa6d2c94f69edcf5327422dbdc
thomahi <thomahi@gmx.de>
2013-09-25 15:34:00 +0200

    buildup classifier rdy

--- MyWekaClassifier/src/myclassifier/MyClassifier.java
+++ MyWekaClassifier/src/myclassifier/MyClassifier.java
@@ -5,7 +5,10 @@
  */
 package myclassifier;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import weka.classifiers.*;
@@ -13,46 +16,52 @@ import weka.core.*;
 
 public class MyClassifier extends Classifier 
 {
-	private Map<String, Double> values = new HashMap<String, Double>();
+	private ArrayList<Map<String, List<Integer>>> attr_values_count;
+	private int yes_count;
+	private int no_count;
+	
     /*
      * This is the method for training the classifier. Add the
      * code needed to train a classifier on the training set,
      * available in the data datastructure.
      */
     public void buildClassifier(Instances data){
-    	values.put("T", (double)data.numInstances());
-    	System.out.println(data.numAttributes());
-    	Map<String, Integer> values = new HashMap<String, Integer>();
-    	int number_attr = data.numAttributes();
+    	int num_inst = data.numInstances();
+    	int num_attr = data.numAttributes();
+    	yes_count = 0;
+    	no_count = 0;
+    	// list of maps, each map belongs to 1 attribute (same index as in data)
+    	// map holds for each attribute value a list
+    	// the list saves in the first position the amount of instances where the attribute belongs to yes (last attribute in data)
+    	// and 2nd the amount of instances where the attribut belongs to no (last attribute in data)
+    	attr_values_count = new ArrayList<Map<String, List<Integer>>>();
     	
+    	for (int i = 0; i < num_attr-2; i++){
+    		Map<String, List<Integer>> new_map = new HashMap<String, List<Integer>>();
+    		attr_values_count.add(new_map);
+    	}
     	
-    	int sunny = 0;
-    	int rainy = 0;
-    	int overcast = 0;
-    	int hot = 0;
-    	int mild = 0;
-    	int cool = 0;
-    	int high = 0;
-    	int normal = 0;
-    	int windy = 0;
-    	int not_windy = 0;
-    	
-    	int sunny_yes = 0;
-    	int rainy_yes = 0;
-    	int overcast_yes = 0;
-    	int hot_yes = 0;
-    	int mild_yes = 0;
-    	int cool_yes = 0;
-    	int high_yes = 0;
-    	int normal_yes = 0;
-    	int windy_yes = 0;
-    	int not_wind_yes = 0;
-    	System.out.println(data.instance(0).stringValue(0));
-    		
-    		
-    	
-    	
-    	
+    	for (int inst_nu = 0; inst_nu < num_inst; inst_nu++){
+    		Instance inst = data.instance(inst_nu);
+    		for (int attr_nu = 0; attr_nu < num_attr-2; attr_nu++){
+    			Map<String, List<Integer>> attr_map = attr_values_count.get(attr_nu);
+    			String attr_val = inst.stringValue(attr_nu);
+    			if (!attr_map.containsKey(attr_val)){
+    				List<Integer> yes_no_list = new ArrayList<Integer>();
+    				yes_no_list.add(0);
+    				yes_no_list.add(0);
+    				attr_map.put(attr_val, yes_no_list);
+    			}
+    			
+    			if (inst.stringValue(num_attr-1).equals("yes")){
+    				yes_count += 1;
+    				attr_map.get(attr_val).set(0, (Integer)attr_map.get(attr_val).get(0)+1); 
+    			} else {
+    				no_count += 1;
+    				attr_map.get(attr_val).set(1, (Integer)attr_map.get(attr_val).get(1)+1);
+    			}
+    		}
+    	}   	
     }
     
     /*
@@ -61,8 +70,9 @@ public class MyClassifier extends Classifier
      * trained classifier.
      */
     public double classifyInstance(Instance inst) throws NoSupportForMissingValuesException {
-        int rnd = (int)(Math.random() * 2);
-        return rnd;
+        double yes_prob;
+        double no_prob;
+        return 0.0d;
     }
     
     /*
